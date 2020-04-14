430dbb0f02ad3532701179de5b6246d1b3ccf367
Vittoria Thomasini <vittoriathomasini@hotmail.com>
2020-04-14 14:21:34 -0300

    Commit change "Entidade"

--- Trabalho/src/Entidade.java
+++ Trabalho/src/Entidade.java
@@ -1,8 +1,5 @@
 public class Entidade {
-	private String Cpf;
-	private String Email;
 	private String NomeCompleto;
-	private String Telefone;
 
 	public Entidade(String name) {
 		NomeCompleto = name;
@@ -17,11 +14,4 @@ public class Entidade {
 		return NomeCompleto;
 	}
 
-	public void setEmail(String email) {
-		Email = email;
-	}
-
-	public String getEmail() {
-		return Email;
-	}
 }
