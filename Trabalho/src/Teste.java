c316a29d26a708a786410e8d2effefaa1329a568
Quarto David <Quarto David@192.168.25.12>
2020-07-05 23:15:52 -0300

    ok

---
+++ Trabalho/src/Teste.java
@@ -0,0 +1,10 @@
+//easy boy
+public class Teste {
+	
+	//criar classes midia, serie e categoria
+	//MidiaControle, SerieControle, CategoriaControle
+	//MidiaApresentacao, SerieApresentacao, CategoriaApresentacao
+		
+	
+		
+}
